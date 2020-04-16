c29fa66509ecf1910180a69a48cc888cfc003d95
Pawlik123 <elzbieta1@opoczta.pl>
2020-04-16 14:38:23 +0200

    Notowania cen złota

--- gold_analyzer/Analyzer.java
+++ gold_analyzer/Analyzer.java
@@ -3,25 +3,38 @@ package gold_analyzer;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Analyzer {
 	
+
 	public static void main(String[] args){
 		ParaseJSON url = new ParaseJSON("http://api.nbp.pl/api/cenyzlota");
 		JSONArray x =url.getJSONArray();
 		System.out.println(x);
 		
+		Scanner in=new Scanner(System.in);
+		System.out.println("Podaj notowania cen z³ota");
+		int liczba=in.nextInt();
+		ParaseJSON url2=new ParaseJSON("https://api.nbp.pl/api/cenyzlota/last/" + liczba);
+		JSONArray g=url2.getJSONArray();
+		System.out.println(g);
+	
 		 double a=whatIsThePriceOfgold(x);
 		//System.out.println("Aktualna cena zlota to: " + a);
 		 
 		 String h=whatIsTheDate(x);
-		 //System.out.println("Z dnia: "+ h);
+		// System.out.println("Z dnia: "+ h);
 		 
 		System.out.println("Aktualna cena zlota to: " + a + " z dnia: "+ h );
-	
+		
+		double r=howAseriesOfRecentGoldQuotations(g,liczba);
+		System.out.println("Seria ostatnich notowan to " + r );		
+		System.out.println("Œrednia z ostatnich " + liczba + "notowan jest równa " + r/liczba);
 	}
 	
 	public static double whatIsThePriceOfgold(JSONArray y) {
@@ -35,10 +48,16 @@ public class Analyzer {
 		String data=obj.getString("data");
 		return data;	
 	}
+	
+	public static double howAseriesOfRecentGoldQuotations(JSONArray f,int liczba){
+		
+		double p=0;
+		for(int y=0; y<liczba; y++){
+			double cena = f.getJSONObject(y).getDouble("cena");
+			p=p+cena;
+		}
+		
+		return p;	
+	}
+	
 }
-
-
-
-
-
-
